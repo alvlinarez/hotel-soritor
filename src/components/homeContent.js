451799947b5f9434bdee9a3fd6f3c17eb1592715
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-06-08 19:22:19 -0500

    Content of home page added

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const HomeDivContent = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+  p {
+    line-height: 2;
+  }
+`;
 
 const HomeContent = () => {
   const info = useStaticQuery(graphql`
     query {
-      allDatoCmsPage(filter: { slug: { eq: "home" } }){
+      allDatoCmsPage(filter: { slug: { eq: "home" } }) {
         nodes {
           title
           content
           image {
             fluid {
-#              srcSet
+              #              srcSet
               ...GatsbyDatoCmsFluid
             }
           }
@@ -18,10 +36,22 @@ const HomeContent = () => {
       }
     }
   `);
-  console.log(info);
+  const { title, content, image } = info.allDatoCmsPage.nodes[0];
   return (
     <>
-     <h2>Page Title</h2>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {title}
+      </h2>
+      <HomeDivContent>
+        <p>{content}</p>
+        <Image fluid={image.fluid} alt={title} />
+      </HomeDivContent>
     </>
   );
 };
