164b81a6628ccf2cb1d23d0e2a568d9888e9c612
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-06-09 21:16:48 -0500

    Footer added to the project

---
+++ src/components/footer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+import Navigation from './nav';
+
+const HomeLink = styled(Link)`
+  color: #ffffff;
+  text-align: center;
+  text-decoration: none;
+`;
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <>
+      <footer
+        css={css`
+          background-color: rgba(44, 62, 80);
+          margin-top: 5rem;
+          padding: 1rem;
+        `}
+      >
+        <div
+          css={css`
+            max-width: 1200px;
+            margin: 0 auto;
+            @media (min-width: 768px) {
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+            }
+          `}
+        >
+          <Navigation />
+          <HomeLink to={'/'}>
+            <h1>Hotel Soritor</h1>
+          </HomeLink>
+        </div>
+      </footer>
+      <p
+        css={css`
+          text-align: center;
+          color: #fff;
+          background-color: rgb(33, 44, 55);
+          margin: 0;
+          padding: 1rem;
+        `}
+      >
+        Hotel Soritor. All rights reserved {year} &copy;
+      </p>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Global, css } from '@emotion/core';
 import Header from './header';
+import Footer from './footer';
 
 const Layout = (props) => {
   return (
@@ -57,6 +58,7 @@ const Layout = (props) => {
       </Helmet>
       <Header />
       {props.children}
+      <Footer />
     </>
   );
 };
